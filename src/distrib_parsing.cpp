bd6fe8ff06a16976b8be0a4519637631245102a9
Iago-lito <iago-lito@etak>
2022-03-29 11:28:57 +0200

    Read distribution file to string.

--- src/distrib_parsing.cpp
+++ src/distrib_parsing.cpp
@@ -1,8 +1,26 @@
 #include "distrib_parsing.hpp"
 
+#include <fstream>
+#include <ios>
+
 namespace distribution {
 
+// https://stackoverflow.com/a/116220/3719101
+auto read_file(std::string_view path) -> std::string {
+  constexpr auto read_size = std::size_t(4096);
+  auto stream = std::ifstream(path.data());
+  stream.exceptions(std::ios_base::badbit);
+  auto out = std::string();
+  auto buf = std::string(read_size, '\0');
+  while (stream.read(&buf[0], read_size)) {
+    out.append(buf, 0, stream.gcount());
+  }
+  out.append(buf, 0, stream.gcount());
+  return out;
+}
+
 Map parse_file(const std::string_view filename, const Areas& areas) {
+  const auto file{read_file(filename)};
   return {};
 };
 
