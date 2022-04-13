7fc05ed31aee12eda145e1b89f9ba4fcaa035d07
Iago-lito <iago-lito@etak>
2022-04-13 16:27:03 +0200

    Parse mandatory header in a\s file.

--- src/distrib_parsing_areas.cpp
+++ src/distrib_parsing_areas.cpp
@@ -5,8 +5,24 @@
 namespace distribution {
 
 Map areas_parse(Lexer& lexer, const Areas& config_areas) {
-  std::cerr << "a\\s unimplemented." << std::endl;
-  exit(-1);
+  // Construct the transposed version {area: '10101'},
+  // then de-transpose at the end.
+  Map tmap{};
+
+  // Mandatory header: the total list of species about to be processed.
+  auto next{lexer.step()};
+  std::vector<std::string> species{};
+  while (next.has_value()) {
+    species.emplace_back(*next.token);
+    next = lexer.step();
+  }
+  if (species.empty()) {
+    std::cerr << "Error: no species lines provided in distribution file header "
+              << "(sought because of 'a\\s' specification)." << std::endl;
+    next.source_and_exit();
+  }
+
+  return tmap;
 }
 
 } // namespace distribution
