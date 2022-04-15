63062357d8ea960e963ebceaf16e34c2288a72f7
Iago-lito <iago-lito@etak>
2022-04-15 15:14:08 +0200

    Copy base test input files to temporary dir.

--- tests/input/main.py
+++ tests/input/main.py
@@ -6,6 +6,7 @@ Assume it's run from the repo.
 from subprocess import Popen, PIPE, STDOUT
 from multiprocessing import cpu_count
 from pathlib import Path
+import shutil as shu
 import os
 
 
@@ -39,17 +40,35 @@ if os.system(f"make -j {cpu_count()}"):
     raise Exception("Could not compile project.")
 binary = Path(build_folder, "decx")
 
+# Check that base test input files exist.
+test_folder = ["tests", "input"]
+test_folder = Path(root_folder, *test_folder)
+if not test_folder.exists():
+    raise Exception(f"Could not find input tests folder {test_folder}.")
+os.chdir(test_folder)
+
 # Construct temporary test environment.
-temp_folder = ["tests", "input", "temp"]
-temp_folder = Path(root_folder, *temp_folder)
+temp_folder = "temp"
+temp_folder = Path(test_folder, temp_folder)
 if temp_folder.exists():
     raise Exception(f"Temporary folder {temp_folder} already exists: won't replace.")
 temp_folder.mkdir()
 os.chdir(temp_folder)
 
-# TODO: run tests here.
+# Construct copies to be modified during tests.
+input_files = ["config.toml", "newick.tre", "distrib_legacy.txt", "adjacency.txt"]
+for file in input_files:
+    path = Path(test_folder, "dummy_files", file)
+    if not os.path.isfile(path):
+        raise Exception(f"Could not find test input file {path}.")
+    shu.copy(path, temp_folder)
+
+# Run the program with config files.
+config = input_files[0]
+flag = "--check-distribution-file-parsing"
+cmd = f"{binary} {config} {flag}"
+os.system(cmd)
 
 # Remove temporary test environment.
 os.chdir(root_folder)
-os.rmdir(temp_folder)
-
+shu.rmtree(temp_folder)
